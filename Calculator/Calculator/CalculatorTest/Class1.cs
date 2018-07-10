9ddbb60005a77a8bf26bd9631dd20505b6739b37
SokolkovGleb <nub.azazaza@mail.ru>
2018-07-10 12:43:29 +0600

    Simple test

---
+++ Calculator/Calculator/CalculatorTest/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.TwoArguments;
+using NUnit.Framework;
+
+
+namespace CalculatorTest
+{
+    public class Class1
+    {
+
+        [TestFixture]
+        public class AddTests
+        {
+            [Test]
+            
+            public void AddTest()
+            {
+               var calculator = new Add();
+                Assert.AreEqual(4, calculator.Calculate(3,1));
+            }
+
+            [Test]
+            public void SubstractionTest()
+            {
+                var calculator = new Substraction();
+                Assert.AreEqual(2, calculator.Calculate(3, 1));
+            }
+
+            [Test]
+            public void MultiplyTest()
+            {
+                var calculator = new Multiply();
+                Assert.AreEqual(2, calculator.Calculate(2, 1));
+            }
+            [Test]
+            public void DivisionTest()
+            {
+                var calculator = new Division();
+                Assert.AreEqual(3, calculator.Calculate(3, 1));
+            }
+
+        }
+
+
+    }
+}
